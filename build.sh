f562426075be64bd0a84df5cb791b76b072c7468
riktenx <riktenx@gmail.com>
2025-03-09 19:30:27 +0000

    add generate script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "generating filterscape.rs2f..."
+
+echo "" > filterscape.rs2f
+
+cat cox/module.rs2f >> filterscape.rs2f
+cat toa/module.rs2f >> filterscape.rs2f
+cat value/module.rs2f >> filterscape.rs2f
+cat currency/module.rs2f >> filterscape.rs2f
+cat unique/module.rs2f >> filterscape.rs2f
+cat clue/mod_clue.rs2f >> filterscape.rs2f
+cat herb/module.rs2f >> filterscape.rs2f
+
+echo "done."

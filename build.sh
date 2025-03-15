78b1e87677a41f9deb60763f0e5ca41a2b2ab86d
riktenx <riktenx@gmail.com>
2025-03-15 03:47:40 +0000

    add potions

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ cat toa/module.rs2f >> filterscape.rs2f
 cat unique/module.rs2f >> filterscape.rs2f
 cat value/module.rs2f >> filterscape.rs2f
 cat currency/module.rs2f >> filterscape.rs2f
+cat potion/module.rs2f >> filterscape.rs2f
 cat clue/mod_clue.rs2f >> filterscape.rs2f
 cat herb/module.rs2f >> filterscape.rs2f
 

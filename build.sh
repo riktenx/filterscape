19408fb0645e78270a4576aa697e16727ce747a4
riktenx <riktenx@gmail.com>
2025-03-25 22:52:39 +0000

    add highlight groups

--- build.sh
+++ build.sh
@@ -4,13 +4,14 @@ echo "generating filterscape.rs2f..."
 
 echo "" > filterscape.rs2f
 
+cat highlights/module.rs2f >> filterscape.rs2f
 cat cox/module.rs2f >> filterscape.rs2f
 cat toa/module.rs2f >> filterscape.rs2f
 cat unique/module.rs2f >> filterscape.rs2f
 cat value/module.rs2f >> filterscape.rs2f
 cat currency/module.rs2f >> filterscape.rs2f
 cat potion/module.rs2f >> filterscape.rs2f
-cat clue/mod_clue.rs2f >> filterscape.rs2f
+cat clue/module.rs2f >> filterscape.rs2f
 cat herb/module.rs2f >> filterscape.rs2f
 
 echo "done."

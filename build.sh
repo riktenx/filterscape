43b718022e85eb36d5cf221653e8183f24d7b289
riktenx <riktenx@gmail.com>
2025-03-28 06:22:25 +0000

    module/

--- build.sh
+++ build.sh
@@ -4,23 +4,23 @@ echo "generating filterscape.rs2f..."
 
 echo "" > filterscape.rs2f
 
-cat highlights/module.rs2f >> filterscape.rs2f
-cat hides/module.rs2f >> filterscape.rs2f
-cat boss/generalgraardor/module.rs2f >> filterscape.rs2f
-cat boss/commanderzilyana/module.rs2f >> filterscape.rs2f
-cat boss/kriltsutsaroth/module.rs2f >> filterscape.rs2f
-cat boss/kreearra/module.rs2f >> filterscape.rs2f
-cat boss/nex/module.rs2f >> filterscape.rs2f
-cat boss/vorkath/module.rs2f >> filterscape.rs2f
-cat zulrah/module.rs2f >> filterscape.rs2f
-cat muspah/module.rs2f >> filterscape.rs2f
-cat cox/module.rs2f >> filterscape.rs2f
-cat toa/module.rs2f >> filterscape.rs2f
-cat unique/module.rs2f >> filterscape.rs2f
-cat potion/module.rs2f >> filterscape.rs2f
-cat value/module.rs2f >> filterscape.rs2f
-cat currency/module.rs2f >> filterscape.rs2f
-cat clue/module.rs2f >> filterscape.rs2f
-cat herb/module.rs2f >> filterscape.rs2f
+cat module/highlights/module.rs2f >> filterscape.rs2f
+cat module/hides/module.rs2f >> filterscape.rs2f
+cat module/boss/generalgraardor/module.rs2f >> filterscape.rs2f
+cat module/boss/commanderzilyana/module.rs2f >> filterscape.rs2f
+cat module/boss/kriltsutsaroth/module.rs2f >> filterscape.rs2f
+cat module/boss/kreearra/module.rs2f >> filterscape.rs2f
+cat module/boss/nex/module.rs2f >> filterscape.rs2f
+cat module/boss/vorkath/module.rs2f >> filterscape.rs2f
+cat module/zulrah/module.rs2f >> filterscape.rs2f
+cat module/muspah/module.rs2f >> filterscape.rs2f
+cat module/cox/module.rs2f >> filterscape.rs2f
+cat module/toa/module.rs2f >> filterscape.rs2f
+cat module/unique/module.rs2f >> filterscape.rs2f
+cat module/potion/module.rs2f >> filterscape.rs2f
+cat module/value/module.rs2f >> filterscape.rs2f
+cat module/currency/module.rs2f >> filterscape.rs2f
+cat module/clue/module.rs2f >> filterscape.rs2f
+cat module/herb/module.rs2f >> filterscape.rs2f
 
 echo "done."

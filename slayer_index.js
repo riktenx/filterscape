5a14243f9460bea00981925099d7a0e690411d4d
riktenx <riktenx@gmail.com>
2025-04-27 15:34:08 +0000

    smokedevil: correct map area

--- slayer_index.js
+++ slayer_index.js
@@ -1,21 +1,21 @@
-import { cmdGenerateModGroup } from './cmd.js';
+import { cmdGenerateModGroup } from "./cmd.js";
 
 const setPlane = (mapAreas, key, plane) => {
   mapAreas[key][2] = plane;
   mapAreas[key][5] = plane;
-}
+};
 
 const index = [
   {
-    name: 'Bloodveld',
-    url: 'https://oldschool.runescape.wiki/w/Bloodveld',
+    name: "Bloodveld",
+    url: "https://oldschool.runescape.wiki/w/Bloodveld",
     transform: {
       updateMapAreas: (areas) => {
         // wiki is wrong
-        setPlane(areas, '[[Slayer Tower]] (basement)', 3);
+        setPlane(areas, "[[Slayer Tower]] (basement)", 3);
 
         // wiki has a formatting quirk that breaks the parser
-        areas['Slayer tower (upstairs)'] = [3404, 3556, 1, 3429, 3579, 1];
+        areas["Slayer tower (upstairs)"] = [3404, 3556, 1, 3429, 3579, 1];
       },
       getDefaults: () => {
         return {
@@ -26,26 +26,23 @@ const index = [
             "Mithril sq shield",
             "Mithril chainbody",
           ],
-          "Other": [
-            "Bones",
-            "Meat pizza",
-          ],
+          Other: ["Bones", "Meat pizza"],
         };
       },
     },
   },
   {
-    name: 'Nechryael',
-    url: 'https://oldschool.runescape.wiki/w/Nechryael',
+    name: "Nechryael",
+    url: "https://oldschool.runescape.wiki/w/Nechryael",
     transform: {
       updateMapAreas: (areas) => {
-        setPlane(areas, '[[Slayer Tower]] Basement', 3); // wiki is wrong
+        setPlane(areas, "[[Slayer Tower]] Basement", 3); // wiki is wrong
       },
     },
   },
   {
-    name: 'Vyre',
-    url: 'https://oldschool.runescape.wiki/w/Vyrewatch_Sentinel',
+    name: "Vyre",
+    url: "https://oldschool.runescape.wiki/w/Vyrewatch_Sentinel",
     transform: {
       getDefaults: () => {
         return {};
@@ -53,12 +50,12 @@ const index = [
     },
   },
   {
-    name: 'Aberrant spectre',
-    url: 'https://oldschool.runescape.wiki/w/Aberrant_spectre',
+    name: "Aberrant spectre",
+    url: "https://oldschool.runescape.wiki/w/Aberrant_spectre",
     transform: {
       updateDropTable: (table) => {
-        delete table['Other'];
-        delete table['Tertiary'];
+        delete table["Other"];
+        delete table["Tertiary"];
       },
       getDefaults: () => {
         return {
@@ -67,22 +64,18 @@ const index = [
             "Mithril kiteshield",
             "Adamant platelegs",
           ],
-          "Coins": [
-            "Coins",
-          ],
+          Coins: ["Coins"],
         };
       },
     },
   },
   {
-    name: 'Dust devil',
-    url: 'https://oldschool.runescape.wiki/w/Dust_devil',
+    name: "Dust devil",
+    url: "https://oldschool.runescape.wiki/w/Dust_devil",
     transform: {
       getDefaults: (_) => {
         return {
-          "100%": [
-            "Bones",
-          ],
+          "100%": ["Bones"],
           "Weapons and armour": [
             "Adamant axe",
             "Rune dagger",
@@ -91,25 +84,25 @@ const index = [
             "Air battlestaff",
             "Earth battlestaff",
           ],
-          "Runes and ammunition": [
-            "Rune arrow",
-          ],
-          "Other": [
-            "Ugthanki kebab",
-          ],
+          "Runes and ammunition": ["Rune arrow"],
+          Other: ["Ugthanki kebab"],
         };
       },
     },
   },
   {
-    name: 'Smoke devil',
-    url: 'https://oldschool.runescape.wiki/w/Smoke_devil',
+    name: "Smoke devil",
+    url: "https://oldschool.runescape.wiki/w/Smoke_devil",
     transform: {
+      updateMapAreas: (table) => {
+        // there are some strays in thermy's room so this breaks the thermy
+        // boss module, just adjust it manually
+        delete table["[[Smoke Devil Dungeon]]"];
+        table["[[Smoke Devil Dungeon]]"] = [2377, 9412, 0, 2427, 9471, 0];
+      },
       getDefaults: (_) => {
         return {
-          "100%": [
-            "Ashes"
-          ],
+          "100%": ["Ashes"],
           "Weapons and armour": [
             "Adamant battleaxe",
             "Rune dagger",
@@ -125,46 +118,35 @@ const index = [
             "Fire rune",
             "Air rune",
             "Soul rune",
-            "Rune arrow"
+            "Rune arrow",
           ],
-          "Coins": [
-            "Coins"
-          ],
-          "Other": [
+          Coins: ["Coins"],
+          Other: [
             "Shark",
             "Steel bar",
             "Magic logs",
             "Coal",
             "Adamantite bar",
             "Crossbow string",
-            "Ugthanki kebab"
+            "Ugthanki kebab",
           ],
         };
       },
     },
   },
   {
-    name: 'Skeletal wyvern',
-    url: 'https://oldschool.runescape.wiki/w/Skeletal_Wyvern',
+    name: "Skeletal wyvern",
+    url: "https://oldschool.runescape.wiki/w/Skeletal_Wyvern",
   },
   {
-    name: 'Gargoyle',
-    url: 'https://oldschool.runescape.wiki/w/Gargoyle',
+    name: "Gargoyle",
+    url: "https://oldschool.runescape.wiki/w/Gargoyle",
     transform: {
       getDefaults: () => {
         return {
-          "Weapons and armour": [
-            "Adamant platelegs",
-            "Adamant boots",
-          ],
-          "Runes": [
-            "Fire rune",
-          ],
-          "Materials": [
-            "Gold ore",
-            "Pure essence",
-            "Gold bar",
-          ],
+          "Weapons and armour": ["Adamant platelegs", "Adamant boots"],
+          Runes: ["Fire rune"],
+          Materials: ["Gold ore", "Pure essence", "Gold bar"],
         };
       },
       preScript: (scope) => {
@@ -179,11 +161,11 @@ CONST_${scope}_RULE (VAR_${scope}_BOOLEAN_HIDESMALLCOINS && name:"Coins" && valu
 }
 `;
       },
-    }
+    },
   },
   {
-    name: 'Abyssal demon',
-    url: 'https://oldschool.runescape.wiki/w/Abyssal_demon',
+    name: "Abyssal demon",
+    url: "https://oldschool.runescape.wiki/w/Abyssal_demon",
     transform: {
       getDefaults: () => {
         return {
@@ -193,47 +175,34 @@ CONST_${scope}_RULE (VAR_${scope}_BOOLEAN_HIDESMALLCOINS && name:"Coins" && valu
             "Black axe",
             "Mithril kiteshield",
           ],
-          "Runes": [
-            "Air rune",
-            "Chaos rune",
-            "Blood rune",
-            "Law rune",
-          ],
-          "Materials": [
-            "Pure essence",
-            "Adamantite bar",
-          ],
-          "Coins": [
-            "Coins",
-          ],
+          Runes: ["Air rune", "Chaos rune", "Blood rune", "Law rune"],
+          Materials: ["Pure essence", "Adamantite bar"],
+          Coins: ["Coins"],
         };
       },
     },
   },
   {
-    name: 'Araxyte',
-    url: 'https://oldschool.runescape.wiki/w/Araxyte',
+    name: "Araxyte",
+    url: "https://oldschool.runescape.wiki/w/Araxyte",
     transform: {
-      updateMapAreas: (table) => { // wiki source is cooked here, just do our own
-        delete table['[[Morytania Spider Cave]] (task-only area)'];
-        table['[[Morytania Spider Cave]]'] = [
-          3653, 9793, 0, 3710, 9868, 0
-        ];
+      updateMapAreas: (table) => {
+        // wiki source is cooked here, just do our own
+        delete table["[[Morytania Spider Cave]] (task-only area)"];
+        table["[[Morytania Spider Cave]]"] = [3653, 9793, 0, 3710, 9868, 0];
       },
       updateDropTable: (table) => {
-        table['Other'] = table['Other']
-          .filter(item => item !== 'Araxyte venom sack');
+        table["Other"] = table["Other"].filter(
+          (item) => item !== "Araxyte venom sack"
+        );
       },
       getDefaults: () => {
         return {
-          "Weapons and armour": [
-            "Adamant longsword",
-            "Adamant battleaxe",
-          ],
+          "Weapons and armour": ["Adamant longsword", "Adamant battleaxe"],
         };
       },
     },
   },
 ];
 
-await cmdGenerateModGroup('slayer', index);
+await cmdGenerateModGroup("slayer", index);
